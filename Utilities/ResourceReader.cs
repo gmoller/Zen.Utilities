002137ee27475c20c616ca139bb517dbd9a11a12
Greg Moller <greg.moller@gmail.com>
2020-11-05 21:06:59 -0800

    Add resourcereader

---
+++ Utilities/ResourceReader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace Zen.Utilities
+{
+    public static class ResourceReader
+    {
+        public static string ReadResource(string resourceName, Assembly assemblyToReadFrom)
+        {
+            using var stream = assemblyToReadFrom.GetManifestResourceStream(resourceName);
+            using var reader = new StreamReader(stream ?? throw new InvalidOperationException($"Failed to get stream for [{resourceName}] from Assembly [{assemblyToReadFrom.FullName}]"));
+            var result = reader.ReadToEnd();
+
+            return result;
+        }
+    }
+}
---
+++ UtilitiesTests/ResourceReaderTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using NUnit.Framework;
+using Zen.Utilities;
+
+namespace Zen.UtilitiesTests
+{
+    public class ResourceReaderTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Embedded_resource_from_assembly_can_be_read()
+        {
+            var text = ResourceReader.ReadResource("Zen.UtilitiesTests.TestFile.txt", Assembly.GetExecutingAssembly());
+
+            Assert.AreEqual("Alex and Marty are the greatest Schnauzers that ever lived.", text);
+        }
+    }
+}
